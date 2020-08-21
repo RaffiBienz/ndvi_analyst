89b02e8c329adb3eb6a3b29db307dbf3b8862897
RaffiBienz <42611754+RaffiBienz@users.noreply.github.com>
2020-08-21 11:21:40 +0200

    cloud and edge effects removal added.

--- NDVI_analyst.R
+++ NDVI_analyst.R
@@ -16,12 +16,17 @@ rasterOptions(tmpdir= paste0(getwd(),"/temp"),todisk=TRUE, progress="text")#prog
 ######################################################################################################################################################################################
 # Forest delination
 wa <- readOGR(dsn="example_data",layer = "wa_example",encoding = "ESRI Shapefile")
-wa <- wa[wa@data$Shape_STAr>20,] # Remove small polygons
+wa <- wa[area(wa)>20,] # Remove small polygons
 wa_agg <- aggregate(wa,dissolve=T) # Dissolve polygons
 
 # Area of interest delination
 kt <- readOGR(dsn="example_data",layer = "kt",encoding = "ESRI Shapefile")
 
+# Areas with clouds
+remove_clouds <- FALSE # If areas with clouds should be ignored, create shapefile with clouds and set TRUE.
+if (remove_clouds){
+  clouds <- readOGR(dsn="data",layer = "clouds",encoding = "ESRI Shapefile")
+}
 
 # Import of Sentinel 2 data (first the rasters need to be projected into the correct coordinate system in a GIS)
 b4_time1 <- raster("example_data/190617_b04_ex.tif")
@@ -51,28 +56,58 @@ ndvi_diff_kt <- crop(ndvi_diff,kt)
 
 # Clip to forest delination
 if (!file.exists("temp/wa_ras.tif")){
-  wa_ras <- rasterize(wa,ndvi_diff_kt,mask=T)
+  wa_ras <- rasterize(wa_agg,ndvi_diff_kt,mask=T)
   wa_ras[!is.na(wa_ras[])] <- 1
   writeRaster(wa_ras,"temp/wa_ras.tif")
 }
 wa_ras <- raster("temp/wa_ras.tif")
-
 ndvi_diff_wa <- ndvi_diff_kt * wa_ras # This method was used, because raster::mask() did not work properly with polygons which have holes in them.
+
+# Remove areas with clouds
+if (remove_clouds){
+  if (!file.exists("temp/cloud_ras.tif")){
+    cloud_ras <- rasterize(clouds,ndvi_diff_kt,mask=T)
+    cloud_ras[is.na(cloud_ras[])] <- 1
+    cloud_ras[cloud_ras[]<1] <- 0
+    writeRaster(cloud_ras,"temp/cloud_ras.tif",overwrite=T)
+  }
+  cloud_ras <- raster("temp/cloud_ras.tif")
+  ndvi_diff_wa <- ndvi_diff_wa * cloud_ras # This method was used, because raster::mask() did not work properly with polygons which have holes in them.
+}
 
 writeRaster(ndvi_diff_wa,"result/ndvi_diff_wa_180819_190617.tif", overwrite=TRUE)
 removeTmpFiles(h=0)
 
 ######################################################################################################################################################################################
-#### Data analysis ####
+#### Analysis ####
 ######################################################################################################################################################################################
+threshold <- -0.08 # Which NDVI reduction is regarded as a change, 
 ndvi_diff_wa <- raster("result/ndvi_diff_wa_180819_190617.tif")
 
-threshold <- -0.08 # Which NDVI reduction is regarded as a change
-
 length(which(!is.na(ndvi_diff_wa[])))/100 # forest area 
-length(which(ndvi_diff_wa[]< (threshold)))/100 # Affected forest area in ha with threshold -0.08
-length(which(ndvi_diff_wa[]< (threshold)))/length(which(!is.na(ndvi_diff_wa[])))*100 # Affected forest area in % with threshold -0.08
+length(which(ndvi_diff_wa[]< threshold))/100 # Affected forest area in ha with threshold
+length(which(ndvi_diff_wa[]< threshold))/length(which(!is.na(ndvi_diff_wa[])))*100 # Affected forest area in % with threshold
 
+
+######################################################################################################################################################################################
+#### Remove edge effects and single pixels ####
+######################################################################################################################################################################################
+threshold <- -0.08 # Which NDVI reduction is regarded as a change
+ndvi_diff_wa <- raster("result/ndvi_diff_wa_180819_190617.tif")
+
+ndvi_recl <- reclassify(ndvi_diff_wa,matrix(c(-10000,threshold,1,threshold,1000,0),byrow = T,ncol = 3))
+#writeRaster(ndvi_recl,"wd/ndvi_recl.tif", overwrite=TRUE)
+
+ndvi_focal <- focal(ndvi_recl,matrix(rep(1,9),ncol = 3),fun=modal)
+writeRaster(ndvi_focal,"result/ndvi_diff_wa_focal_180819_190617.tif", overwrite=TRUE)
+
+
+######################################################################################################################################################################################
+#### Analysis without edge effects ####
+######################################################################################################################################################################################
+ndvi_focal <- raster("result/ndvi_diff_wa_focal_180819_190617.tif")
+length(which(ndvi_focal[]==1))/100 # Affected forest area in ha with threshold
+length(which(ndvi_focal[]==1))/length(which(!is.na(ndvi_focal[])))*100 # Affected forest area in % with threshold
 
 
 
